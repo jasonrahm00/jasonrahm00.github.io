d0b4a6ad93ad5c1b71378269bfbfcc0030b62f43
Jason Rahm <jasonrahm@gmail.com>
2023-04-07 16:36:06 -0600

    commnets added

--- part1/boston-walk/walkboston.js
+++ part1/boston-walk/walkboston.js
@@ -2,25 +2,31 @@ import {boston} from './data.js';
 
 function renderPosts(people) {
   const len = people.length;
-  let topFive = people.sort((a, b) => a[11] - b[11]).slice(len - 5)
-  let topEmployees = people.filter(person => person[11] >= 200000)
+  const topFive = people.sort((a, b) => a[11] - b[11]).slice(len - 5); // Top Five Earners
+  const topEmployees = people.filter(person => person[11] >= 200000); // Anyone making over @200k
   
+  // Generate HTML list items with person data
   function generateList(arr) {
     let html = '';
+
+    // Use forEach to iterate over array and built HTML using template literal syntax
+      // Person's name (person[8] element) formatted so it reads firstname + lastname
     arr.forEach(person => {
       html += `
         <li class="post">
           <b>${person[8].split(',').reverse().join(' ')}</b>: $${person[11]}
         </li>
-      `
-    })
-    return html
+      `;
+    });
+
+    return html;
   }
 
-  // TODO: add code to display the html variable inside a ul element with id="data"
-  // Hint: you can use the container parameter's innerHTML property to insert Html tags
-  document.getElementById('top-five').innerHTML += `<ul id="data">${generateList(topFive)}</ul>`
-  document.getElementById('top-employees').innerHTML += `<ul id="topEmployees">${generateList(topEmployees)}</ul>`
+  // Add top five earners to the indicated element
+  document.getElementById('top-five').innerHTML += `<ul>${generateList(topFive)}</ul>`;
+
+  // Add >200k earners to the indicated element
+  document.getElementById('top-employees').innerHTML += `<ul>${generateList(topEmployees)}</ul>`;
 }
 
 renderPosts(boston.data);
