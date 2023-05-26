bc2609f5015080b4702c219aad2fff1c8726e98b
Jason Rahm <jasonrahm@gmail.com>
2023-05-25 21:58:05 -0600

    main nav styled

--- components/mainNav.js
+++ components/mainNav.js
@@ -1,4 +1,4 @@
-const baseUrl = 'jasonrahm00.github.io'
+const currentPage = window.location.href
 
 export class MainNav extends HTMLElement {
   constructor() {
@@ -7,20 +7,23 @@ export class MainNav extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
-      <nav class="navbar navbar-expand-sm fixed-top bg-dark" data-bs-theme="dark">
-        <div class="container-fluid justify-content-center">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a href="index.html" class="nav-link">
-                <span>Home</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="projects.html" class="nav-link">
-                <span>Projects</span>
-              </a>
-            </li>
-          </ul>
+      <nav class="navbar fixed-top bg-dark" data-bs-theme="dark">
+        <div class="container">
+          <div class="container-fluid d-flex ps-0">
+            <a href="index.html" class="navbar-brand">Jason Rahm</a>
+            <ul class="navbar-nav flex-row">
+              <li class="nav-item">
+                <a href="index.html" class="nav-link p-2">
+                  <span>Home</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="projects.html" class="nav-link p-2">
+                  <span>Projects</span>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     `
